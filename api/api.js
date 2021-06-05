9248024736d583cc593f365b34aed70cfbca315b
AndrewLaird <lairdandrew11@gmail.com>
2021-06-05 10:37:37 -0700

    making the api work for multiple users at once

--- api/api.js
+++ api/api.js
@@ -4,45 +4,74 @@ const app = express();
 const multer = require('multer');
 const router = express.Router()
 const fs = require('fs');
+const path = require('path');
 
 const {spawn} = require('child_process');
 
-var upload = multer({ dest: 'api/ChessTutorModels/data/input_images' })
+// This will strictly increase, problem if we have many many uploads between resets
+// to combat this we will reset it every 10000
+var files_uploaded = 1;
+var storage = multer.diskStorage({ 
+    destination: function(req, file, cb){
+        files_uploaded = (files_uploaded + 1 % 10000);
+        const path ="./api/ChessTutorModels/data/input_images/"+files_uploaded+"/";
+        fs.mkdirSync(path, { recursive: true })
+        cb(null, path)
+    },
+    filename: function(req, file, cb){
+        original_extension = path.extname(file.originalname);
+        cb(null,files_uploaded + original_extension);
+    },
+});
 
+var upload = multer({
+    storage:storage,
+});
 
 // this is once we've already gotten to api/
 router.post('*', upload.single('chessboard'), function(req, res){
-    console.log('recieved');
     var dataToSend;
     var jsonToSend;
-    //console.log(req.file)
-    const python = spawn('python3', ['./api/ChessTutorModels/get_fen_from_image.py'])
-    //const python = spawn('python3', ['~/personal/StreamlineChessTutor/get_fen_from_image.py'])
+    const file = req.file;
+    const file_folder = path.dirname(file.path);
+    const file_number = files_uploaded
+    const python = spawn('python3', ['./api/ChessTutorModels/get_fen_from_image.py', path.dirname(file.path), files_uploaded])
 
     python.stdout.on('data', function (data) {
         dataToSend = data.toString();
-	console.log(dataToSend);
-        let lines = dataToSend.split('\n')
-        jsonToSend = {
-            'code': lines[0],
-            'crop': lines[1],
-            'fen': lines[2]
-        }
+        console.log(dataToSend);
     });
     // in close event we are sure that stream from child process is closed
     python.on('close', (code) => {
+        // open up the file attached to this
+
         // delete all files in the folder
-        let folder= './api/ChessTutorModels/data/input_images'
-        fs.readdir(folder, (err, files)=>{
+        fs.rm(file_folder, {recursive:true, force:true }, err => {
             if (err) console.log(err);
-                for (const file of files) {
-                    fs.unlink(folder+'/'+file, err => {
-                        if (err) console.log(err);
-                    });
-                }
+        });
+        // we should do this in for the cropped folder too we know what it is, it's files_uploaded
+        fs.rm('api/ChessTutorModels/data/cropped/'+file_number, {recursive:true, force:true}, err => {
+            if (err) console.log(err);
+        });
+        // get the data from the file
+        fs.readFile('api/ChessTutorModels/data/output_files/'+file_number+".txt", (err, data)=>{
+            if(err){
+                console.log(err)
+                return
+            }
+            const file_string = new Buffer.from(data).toString();
+            let lines = file_string.split("\n")
+            jsonToSend = {
+                'code': lines[0],
+                'crop': lines[1],
+                'fen': lines[2]
+            }
+            // send data to browser
+            res.json(jsonToSend)
         })
-        // send data to browser
-        res.json(jsonToSend)
+        fs.rm('api/ChessTutorModels/data/output_files/'+file_number+'.txt', {recursive:true, force:true}, err => {
+            if (err) console.log(err);
+        });
     });
 
 
